01e9c072f60834857500cc98b32d6b588e598f75
Alexander Kuzevanov <alexchampion2003@gmail.com>
2024-09-30 19:45:38 +0500

    initial commit

---
+++ src/test/java/ru/Kuzevanov_Alexander/NauJava/NauJavaApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.Kuzevanov_Alexander.NauJava;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NauJavaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
