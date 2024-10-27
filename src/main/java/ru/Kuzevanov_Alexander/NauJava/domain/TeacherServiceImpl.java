d71ed4e5952a09f88c726ad239ec6d401588aa85
Alexander Kuzevanov <alexchampion2003@gmail.com>
2024-10-27 18:37:02 +0500

    mr review fixes

--- src/main/java/ru/Kuzevanov_Alexander/NauJava/domain/TeacherServiceImpl.java
+++ src/main/java/ru/Kuzevanov_Alexander/NauJava/domain/TeacherServiceImpl.java
@@ -1,11 +1,7 @@
 package ru.Kuzevanov_Alexander.NauJava.domain;
 
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.dao.DataAccessException;
+import jakarta.transaction.Transactional;
 import org.springframework.stereotype.Service;
-import org.springframework.transaction.PlatformTransactionManager;
-import org.springframework.transaction.TransactionStatus;
-import org.springframework.transaction.support.DefaultTransactionDefinition;
 import ru.Kuzevanov_Alexander.NauJava.data.repositories.schedulecell.ScheduleCellRepository;
 import ru.Kuzevanov_Alexander.NauJava.data.repositories.TeacherRepository;
 
@@ -14,32 +10,22 @@ public class TeacherServiceImpl implements TeacherService {
 
     private final ScheduleCellRepository scheduleCellRepository;
     private final TeacherRepository teacherRepository;
-    private final PlatformTransactionManager transactionManager;
 
-    @Autowired
     public TeacherServiceImpl(
             ScheduleCellRepository scheduleCellRepository,
-            TeacherRepository teacherRepository,
-            PlatformTransactionManager transactionManager
+            TeacherRepository teacherRepository
     ) {
         this.scheduleCellRepository = scheduleCellRepository;
         this.teacherRepository = teacherRepository;
-        this.transactionManager = transactionManager;
     }
 
     @Override
+    @Transactional
     public void deleteTeacher(Long id, Boolean isNegative) {
-        TransactionStatus status = transactionManager.getTransaction(new DefaultTransactionDefinition());
-        try {
-            scheduleCellRepository.deleteByTeacher(id);
-            if (isNegative) {
-                throw new IllegalArgumentException("Illegal teacher id");
-            }
-            teacherRepository.deleteById(id);
-            transactionManager.commit(status);
-        } catch (Exception ex) {
-            transactionManager.rollback(status);
-            throw ex;
+        scheduleCellRepository.deleteByTeacher(id);
+        if (isNegative) {
+            throw new IllegalArgumentException("Illegal teacher id");
         }
+        teacherRepository.deleteById(id);
     }
 }
